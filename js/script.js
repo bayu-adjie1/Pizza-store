1594c87acc123a6c284fa303e105ffcb91e499e1
bayu-adjie1 <by.smahiru@gmail.com>
2026-03-12 21:41:16 +0700

    testimonial

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
-// Header/Nav
-
-$("header a").on("click", function () {
-  $(".nav-link").removeClass("active");
-
-  if ($(this).hasClass("nav-link")) {
-    $(this).addClass("active");
-  }
-});
-
-$(".burger").on("click", function () {
-  $(".top").toggleClass("top-on");
-  $(".bottom").toggleClass("bottom-on");
-  $(".mobile-menu").toggleClass("active");
-  
-});
-
-// End Header/Nav
-
 (function(){
+  // Header/Nav
   "use strict";
+
+  $("header a").on("click", function () {
+    $(".nav-link").removeClass("active");
+
+    if ($(this).hasClass("nav-link")) {
+      $(this).addClass("active");
+    }
+  });
+
+  $(".burger").on("click", function () {
+    $(".top").toggleClass("top-on");
+    $(".bottom").toggleClass("bottom-on");
+    $(".mobile-menu").toggleClass("active");
+    
+  });
+
+  // End Header/Nav
 
   AOS.init({
     duration: 700,
@@ -32,5 +31,33 @@ $(".burger").on("click", function () {
   window.addEventListener("scroll", function(){
     circletype.style.transform = "rotate(" + this.window.pageYOffset+ "deg)";
   })
+
+  let tinyslider = function(){
+    let el = document.querySelectorAll('.testimonial-slider');
+
+    if (el.length > 0){
+      let slider = tns({
+        "container": ".testimonial-slider",
+        "item": 1,
+        "center": true,
+        "loop": true,
+        "mouseDrag": true,
+        "slideBy": 1,
+        "axis": "horizontal",
+        "swipeAngel": true,
+        "speed": 700,
+        "nav": true,
+        "controls": false,
+        "responsive": {
+          "900":{
+            "edgePadding": 30,
+            "items": 2
+          }
+        }
+      })
+    }
+  }
+
+  tinyslider();
 
 })();
